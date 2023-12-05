293f8e353fa0e142ba4653de312bfff35366f6aa
Kevin DUONG <20239070+Keimdjo@users.noreply.github.com>
2023-12-05 14:50:50 +0100

    delete the excess table row from the page

--- src/modules/paged-media/tables.js
+++ src/modules/paged-media/tables.js
@@ -16,33 +16,61 @@ class Tables extends Handler {
 		const addedCells = [...page.area.querySelectorAll("td.added")];
 		// check and remove duplicated cell
 		for (const cell of addedCells) {
-			const existingCell = page.area.querySelector(`td[data-ref="${cell.dataset.ref}"]:not(.added)`);
+			const existingCell = page.area.querySelector(
+				`td[data-ref="${cell.dataset.ref}"]:not(.added)`
+			);
 			if (existingCell) {
 				cell.remove();
 			}
 		}
 		const mainContainer = page.area.querySelector("main");
 		if (mainContainer) {
-			const lastElementTable = mainContainer.querySelector(":scope > div[data-type=\"clause\"]:last-child > div[data-type=\"variant\"]:last-child > div:last-child > table, :scope > div[data-type=\"clause\"]:last-child > div[data-type=\"variant\"]:last-child > table:last-child, :scope > div:last-child > table, :scope > table:last-child");
+			const previousPageMainContainer =
+				pageElement.previousElementSibling?.querySelector(
+					".pagedjs_area > .pagedjs_page_content > div main"
+				);
+
+			if (previousPageMainContainer) {
+				const previousPageLastElementTable =
+					previousPageMainContainer.querySelector(
+						":scope > div[data-type=\"clause\"]:last-child > div[data-type=\"variant\"]:last-child > div:last-child > table,\
+					:scope > div[data-type=\"clause\"]:last-child > div[data-type=\"variant\"]:last-child > table:last-child,\
+					:scope > div:last-child > table,\
+					:scope > table:last-child"
+					);
+
+				if (previousPageLastElementTable) {
+					const lastPreviousPageRow =
+						previousPageLastElementTable.querySelector("tbody tr:last-child");
+					let currentRows;
+
+					if (
+						lastPreviousPageRow &&
+						(currentRows = mainContainer.querySelectorAll("tbody tr")) &&
+						(currentRows[1]?.dataset.ref === lastPreviousPageRow.dataset.ref ||
+							(currentRows[0]?.dataset.ref ===
+								lastPreviousPageRow.dataset.ref &&
+								!lastPreviousPageRow.querySelector("td:not(:empty)")))
+					) {
+						// remove last row from previous page as it is overflowing and duplicated
+						const previousTBody = lastPreviousPageRow.parentNode;
+						lastPreviousPageRow.remove();
+						if (previousTBody.childNodes.length === 0) {
+							previousPageLastElementTable.remove();
+						}
+					}
+				}
+			}
+
+			const lastElementTable = mainContainer.querySelector(
+				":scope > div[data-type=\"clause\"]:last-child > div[data-type=\"variant\"]:last-child > div:last-child > table,\
+				:scope > div[data-type=\"clause\"]:last-child > div[data-type=\"variant\"]:last-child > table:last-child,\
+				:scope > div:last-child > table,\
+				:scope > table:last-child"
+			);
 
 			if (lastElementTable) {
 				page.area.style.columnWidth = "auto"; // show the content that slicely "overflowing"
-			}
-
-			const previousPageMainContainer = pageElement.previousElementSibling?.querySelector(".pagedjs_area > .pagedjs_page_content > div main");
-
-			if (previousPageMainContainer) {
-				const previousPageLastElementTable = previousPageMainContainer.querySelector(":scope > div[data-type=\"clause\"]:last-child > div[data-type=\"variant\"]:last-child > div:last-child > table, :scope > div[data-type=\"clause\"]:last-child > div[data-type=\"variant\"]:last-child > table:last-child, :scope > div:last-child > table, :scope > table:last-child");
-
-				if (previousPageLastElementTable) {
-					const currentPageSameFirstTable = mainContainer.querySelector(`:scope > div[data-type="clause"]:first-child > div[data-type="variant"]:first-child > div:first-child > table[data-ref="${previousPageLastElementTable.dataset.ref}"], :scope > div[data-type="clause"]:first-child > div[data-type="variant"]:first-child > table[data-ref="${previousPageLastElementTable.dataset.ref}"]:first-child, :scope > div:first-child > table[data-ref="${previousPageLastElementTable.dataset.ref}"], :scope > table[data-ref="${previousPageLastElementTable.dataset.ref}"]:first-child`);
-					let lastPreviousPageRow;
-
-					if (currentPageSameFirstTable && (lastPreviousPageRow = previousPageLastElementTable.querySelector("tbody tr:last-child")) && currentPageSameFirstTable.querySelector(`tbody tr[data-ref="${lastPreviousPageRow.dataset.ref}"]:nth-child(2)`)) {
-						// remove last row from previous page as it is overflowing and duplicated
-						lastPreviousPageRow.remove();
-					}
-				}
 			}
 		}
 	}
@@ -59,14 +87,16 @@ class Tables extends Handler {
 			maxCells !== -Infinity
 				? rows.map(() => Array(maxCells).fill(undefined))
 				: [];
-	
+
 		for (let rowIdx = 0; rowIdx < rows.length; ++rowIdx) {
 			const row = rows[rowIdx];
 			const cols = row.querySelectorAll(":scope > td");
 			if (!cols) continue;
-	
+
 			for (let colIdx = 0; colIdx < cols.length; ++colIdx) {
-				const posDataIdx = mapPosData[rowIdx]?.findIndex((d) => d === undefined);
+				const posDataIdx = mapPosData[rowIdx]?.findIndex(
+					(d) => d === undefined
+				);
 				const cell = cols[colIdx];
 				const rowSpan = cell.getAttribute("rowspan")
 					? parseInt(cell.getAttribute("rowspan"), 10) ?? 1
@@ -76,7 +106,7 @@ class Tables extends Handler {
 					: 1;
 				for (let spanIdx = 0; spanIdx < rowSpan; ++spanIdx) {
 					const rowPos = rowIdx + spanIdx;
-	
+
 					mapPosData[rowPos].fill(
 						spanIdx === 0 ? colIdx : "x",
 						posDataIdx,
@@ -91,7 +121,7 @@ class Tables extends Handler {
 			}
 		}
 		return cellsPosData;
-	}	
+	}
 }
 
 export default Tables;
